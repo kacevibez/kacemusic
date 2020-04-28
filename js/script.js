c62bf2e06686b8ba72a3e753f81bcb5c9e52cb83
kacevibez <63555327+kacevibez@users.noreply.github.com>
2020-04-28 15:57:13 +0200

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -13,6 +13,88 @@ navLinks.forEach(link => {
     })
 })
 
+// Music modal
+const musicPortfolioItems = document.querySelectorAll(".music-portfolio__item");
+const musicModal = document.getElementById("music-modal");
+
+musicPortfolioItems.forEach(function(item, index, src) {
+    item.addEventListener("click", () => {
+        const musicModalImg = document.getElementById("music-modal-img");
+        const home = document.getElementById("music-modal-home");
+        const download = document.getElementById("music-modal-download");
+        const audioPlayer = document.getElementById("audioPlayer");
+        const background = document.getElementById("music-modal");
+        const caption = document.getElementById("music-modal-caption");
+        
+        audioPlayer.src = musicPortfolioItems[index].dataset.file;
+        musicModalImg.src = musicPortfolioItems[index].dataset.img;
+        download.href = musicPortfolioItems[index].dataset.file;
+        background.style.backgroundImage = `url("${musicPortfolioItems[index].dataset.background}")`;
+        caption.innerHTML = musicPortfolioItems[index].dataset.caption;    
+        musicModal.classList.add("modal-open");
+        
+        home.addEventListener("click", () => {
+            musicModal.classList.remove("modal-open");
+            audioPlayer.src = "";
+        })  
+           
+    })
+})
+
+// Music player
+        const pPause = document.getElementById("play-pause");
+        const progressBar = document.getElementById("progress-bar");
+        const song = document.getElementById("audioPlayer");
+        const currentTime = document.getElementById("currentTime");
+        const durationTime = document.getElementById("durationTime");
+
+        let playing = true;
+        
+        pPause.addEventListener("click", playPause);
+
+        function playPause() {
+          if (playing) {
+            pPause.src = "img/icons/pause.png";
+            song.play();
+            playing = false;
+          } else {
+            pPause.src = "img/icons/play.png"
+            song.pause();
+            playing = true;
+          }
+        }
+
+        function updateProgressValue() {
+            progressBar.max = song.duration;
+            progressBar.value = song.currentTime;
+            currentTime.innerHTML = formatTime(song.currentTime);
+            durationTime.innerHTML = formatTime(song.duration);
+            if (song.duration === song.currentTime) {
+                song.currentTime = 0;
+                playPause();
+            }
+        };
+
+        setInterval(updateProgressValue, 500);
+
+        function changeProgressBar() {
+          song.currentTime = progressBar.value;
+        };
+
+        function formatTime(seconds) {
+            let min = Math.floor((seconds / 60));
+            let sec = Math.floor(seconds - (min * 60));
+            if (sec < 10){ 
+                sec  = `0${sec}`;
+            };
+            return `${min}:${sec}`;
+        };
+
+        function changeProgressBar() {
+            song.currentTime = progressBar.value;
+        };
+
+
 // Video modal
 const videoPortfolioItems = document.querySelectorAll(".video-portfolio__item");
 const videoModal = document.getElementById("video-modal");
@@ -53,12 +135,12 @@ videoPortfolioItems.forEach(function(item, index, src) {
         
         home.addEventListener("click", () => {
             videoModal.classList.remove("modal-open");
+            videoModalVid.src = "";
         })  
     })
 })
 
-
-// Picture slider 
+// Picture modal 
 const picturePortfolioImages = document.querySelectorAll(".picture-portfolio__img");
 const pictureModal = document.getElementById("picture-modal");
 
@@ -100,5 +182,4 @@ picturePortfolioImages.forEach(function(image, index, src) {
         })  
     })
 })
-
 
